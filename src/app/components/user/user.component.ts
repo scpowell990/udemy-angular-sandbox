3ca80bab186b14ca0b4934140e70ce6e7821c22e
scpowell990 <shanepowell990@gmail.com>
2019-07-19 17:14:59 -0400

    Up to Chapter 4

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User'
+@Component({
+    selector: 'app-user',
+
+    //template: '<h2> John Doe</h2>'
+    templateUrl: './user.component.html',//adding the html to the component
+
+    // styles: [`
+    //     h2{
+    //         color: blue
+    //     }`
+    // ]
+    styleUrls: ['./user.component.css'], //adding custom styles from a stye sheet
+})
+export class UserComponent implements OnInit {
+    //properties
+    user: User;
+
+    //methods
+    constructor() {
+        this.user = {
+            firstName: 'john',
+            lastName: 'Doe',
+            age: 30,
+            address: {
+                street: '50 Main St',
+                city: 'Boston',
+                state: 'MA'
+            }
+        }
+    } 
+
+    ngOnInit(){
+        
+    }
+
+}
+
---
+++ src/app/models/User.ts
@@ -0,0 +1,14 @@
+export interface User {
+    firstName: string,
+    lastName: string,
+    age?: number,
+    address?: {
+        street: string,
+        city: string,
+        state: string
+    },
+    isActive?: boolean,
+    registered? : any
+}
+
+// '?' == optional
